1ba07aa9f33361414eff71c08d5cbbe2398d3b30
hrvrbhanu01 <dixitbhanu16048050@gmail.com>
2023-10-27 10:56:20 +0530

    st

---
+++ go.mod
@@ -0,0 +1,5 @@
+module Rest-api-Go
+
+go 1.20
+
+require github.com/gorilla/mux v1.8.0 // indirect
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+var profiles []Profile = []Profile{}
+
+type User struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+	Email     string `json:"email"`
+}
+type Profile struct {
+	Department  string `json:"department"`
+	Designation string `json:"designation"`
+	Employee    User   `json:"employee"`
+}
+
+func additem(q http.ResponseWriter, r *http.Request) {
+	var newProfile Profile
+	json.NewDecoder(r.Body).Decode(&newProfile)
+
+	q.Header().Set("Content-Type", "application/json")
+
+	profiles = append(profiles, newProfile)
+
+	json.NewEncoder(q).Encode(profiles)
+}
+
+func getAllProfiles(q http.ResponseWriter, r *http.Request) {
+	q.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(q).Encode(profiles)
+}
+
+func getProfile(q http.ResponseWriter, r *http.Request){
+	var idParam string = mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idParam)  //converting var idParam to integer
+	if err!=nil{
+		q.WriteHeader(400)
+		q.Write([]byte("ID could not be converted into integer."))
+		return
+	}
+
+	if id>=len(profiles){
+		q.WriteHeader(404)
+		q.Write([]byte("No profile found with the given ID."))
+	}
+
+
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/profiles", additem).Methods("Post")
+
+	router.HandleFunc("/profiles", getAllProfiles).Methods("Get")
+
+	router.HandleFunc("/profiles/{id}", "getProfile").Methods("Get")
+
+	http.ListenAndServe(":5000", router)
+}
