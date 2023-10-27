fbd14ba5f3cd6cfd8b7c4523f402441bd3eceb95
hrvrbhanu01 <dixitbhanu16048050@gmail.com>
2023-10-27 11:22:37 +0530

    kk

--- main.go
+++ main.go
@@ -38,21 +38,67 @@ func getAllProfiles(q http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(q).Encode(profiles)
 }
 
-func getProfile(q http.ResponseWriter, r *http.Request){
+func getProfile(q http.ResponseWriter, r *http.Request) {
 	var idParam string = mux.Vars(r)["id"]
-	id, err := strconv.Atoi(idParam)  //converting var idParam to integer
-	if err!=nil{
+	id, err := strconv.Atoi(idParam) //converting var idParam to integer
+	if err != nil {
 		q.WriteHeader(400)
 		q.Write([]byte("ID could not be converted into integer."))
 		return
 	}
 
-	if id>=len(profiles){
+	if id >= len(profiles) {
 		q.WriteHeader(404)
 		q.Write([]byte("No profile found with the given ID."))
+		return
+	}
+	profile := profiles[id]
+	q.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(q).Encode(profile)
+
+}
+
+func updateProfile(q http.ResponseWriter, r *http.Request) {
+	var idParam string = mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idParam) //converting var idParam to integer
+	if err != nil {
+		q.WriteHeader(400)
+		q.Write([]byte("ID could not be converted into integer."))
+		return
 	}
 
+	if id >= len(profiles) {
+		q.WriteHeader(404)
+		q.Write([]byte("No profile found with the given ID."))
+		return
+	}
+	var updatedProfile Profile
+	json.NewDecoder(r.Body).Decode(&updatedProfile) //address where we want to add the updated data i.e. the var=updatedProfile
 
+	profiles[id] = updatedProfile
+
+	q.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(q).Encode(updatedProfile) //q is the response writer
+}
+
+func deleteProfile(q http.ResponseWriter, r *http.Request) {
+	var idParam string = mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idParam) //converting var idParam to integer
+	if err != nil {
+		q.WriteHeader(400)
+		q.Write([]byte("ID could not be converted into integer."))
+		return
+	}
+
+	if id >= len(profiles) {
+		q.WriteHeader(404)
+		q.Write([]byte("No profile found with the given ID."))
+		return
+	}
+
+	profiles = append(profiles[:id], profiles[:id+1]...)
+
+	q.WriteHeader(200)
 }
 
 func main() {
@@ -61,7 +107,11 @@ func main() {
 
 	router.HandleFunc("/profiles", getAllProfiles).Methods("Get")
 
-	router.HandleFunc("/profiles/{id}", "getProfile").Methods("Get")
+	router.HandleFunc("/profiles/{id}", getProfile).Methods("Get")
+
+	router.HandleFunc("/profiles/{id}", updateProfile).Methods("PUT")
+
+	router.HandleFunc("/profiles/{id}", deleteProfile).Methods("DELETE")
 
 	http.ListenAndServe(":5000", router)
 }
